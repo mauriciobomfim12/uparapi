fa79af9099655bd0b8b4aee529f7b6b86daa6d47
mauriciobomfim12 <mauriciobomfimm@gmail.com>
2023-11-29 17:31:03 -0300

    first commit2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { HomeModule } from './Home/home.module';
-import { GraphQLModule } from '@nestjs/graphql';
+import { HomeModule } from './Home/home.module'
 import { HomeResolver } from './serverGraphql/home.resolver';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma_config/prisma.service';
@@ -9,10 +7,6 @@ import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
-    }),
     HomeModule,
     PesquisaModule,
     ProdutoModule,
